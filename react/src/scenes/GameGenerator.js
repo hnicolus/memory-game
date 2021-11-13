79db1d39cbc512b8c26c4ce63801f46bb6fc7fba
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-11-13 11:15:43 +0200

    increase grid size after every two challenges.

--- react/src/scenes/GameGenerator.js
+++ react/src/scenes/GameGenerator.js
@@ -1,23 +1,25 @@
-import React,{useEffect, useState} from "react";
+import React, { useState } from "react";
 import GameSession from "../components/GameSession";
 import utils from "../utils";
 
 const GameGenerator = () => {
-	const [gridSize,setGridSize] =useState( 5);
+	const [gridSize, setGridSize] = useState(5);
 
-	const [challengeSize ,setChallengeSize] = useState(3);
+	const [challengeSize, setChallengeSize] = useState(3);
 	const [gameId, setGameId] = useState(1);
 
 	const cellIds = utils.createArray(gridSize * gridSize);
 	const cellWidth = 100 / gridSize;
 	const challengeCellIds = utils.sampleArray(cellIds, challengeSize);
 
-	const handleNextChallenge = ()=>{
-				setChallengeSize(challengeSize+ 1);
-			setGridSize(gridSize+ 1);
-			setGameId(gameId => gameId + 1);
+	const handleNextChallenge = () => {
+		if (challengeSize % 2 === 0) {
+			setGridSize(gridSize + 1);
+		}
+		setGameId(gameId => gameId + 1);
+		setChallengeSize(challengeSize + 1);
 	}
-	const handleResetGame = ()=>{
+	const handleResetGame = () => {
 		setChallengeSize(3);
 		setGridSize(5);
 		setGameId(gameId => gameId + 1);
