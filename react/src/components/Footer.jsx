59e115b24ebedcfe5f29fef8faa4007a48697ae8
Nicolas <36539498+hnicolus@users.noreply.github.com>
2021-11-13 09:15:28 +0200

    Added Default fall throughs.

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -15,10 +15,12 @@ function Footer({ gameStatus, startGame, countdown }) {
 				return <button onClick={startGame}>Start Game</button>;
 			case GameStatus.CHALLENGE:
 			// fall-through
+				break;
 			case GameStatus.PLAYING:
 				return countdown;
 			case GameStatus.WON:
 			// fall-through
+			break;
 			case GameStatus.LOST: {
 				return (
 					<button
